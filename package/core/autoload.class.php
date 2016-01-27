e3d30ca93ffaf51a41d90b4671af31dadd482c2a
Robbyn <gerhardt@booking-solution.de>
2016-01-28 00:56:28 +0100

    v2.0 Alpha

--- package/core/autoload.class.php
+++ package/core/autoload.class.php
@@ -1,24 +1,42 @@
 <?php
-/*
-    Copyright (C) 2016  <Robbyn Gerhardt>
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+/**
+ *  Copyright (C) 2010 - 2016  <Robbyn Gerhardt>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  @package	Webpackages
+ *  @subpackage core
+ *  @author	    Robbyn Gerhardt <gerhardt@webpackages.de>
+ *  @copyright	Copyright (c) 2010 - 2016, Robbyn Gerhardt (http://www.robbyn-gerhardt.de/)
+ *  @license	http://opensource.org/licenses/gpl-license.php GNU Public License
+ *  @link	    http://webpackages.de
+ *  @since	    Version 2.0.0
+ *  @filesource
+ */
 
 namespace package\core;
 
-
+/**
+ * Zum dynamischen Aufrufen von Klassen
+ *
+ * Mittels dem autoload können klassen leicht initialisiert und definiert werden.
+ *
+ * @package		Webpackages
+ * @subpackage	core
+ * @category	Autoload
+ * @author		Robbyn Gerhardt <gerhardt@webpackages.de>
+ */
 class autoload
 {
 	/**
@@ -34,22 +52,22 @@ class autoload
      * @param bool $isStatic Wenn es sich um eine statische Klasse handelt
 	 * @param array $parameter Parameter für den Konstruktor der Klasse. Er muss ein Assoziatives Array sein
 	 *
-	 * @author Robbyn Gerhardt <gerhardt@webpackages.de>
-	 * @copyright 2010-2016 webpackages
-	 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
-	 * @version 1.0.1
-	 *
-	 * @throws \Exception Wird ausgegeben wenn der Klassennamen leer ist oder die Klasse nicht geladen werden kann
+	 * @throws \Exception Wird ausgegeben wenn der Klassennamen leer ist oder die Klasse nicht geladen werden konnte
 	 * @return mixed Gibt die Klasseninstanz zurück oder bei einer statischen Klasse ein true
      */
 	public static function get($class_name, $namespace = null, $isStatic = false, $parameter = array())
     {
         if(empty($class_name))
         {
-            throw new \Exception('class is not string or empty');
+            throw new \Exception('Error: $class_name is empty');
         }
 
 		$pathToFile =  $class_name.self::CLASS_SUFFIX;
+
+		if(!empty($namespace))
+		{
+			$class_name	=	$namespace.$class_name;
+		}
 
 		//Wenn schon eingebunden, nicht erneut laden
 		if(class_exists($class_name) === false)
@@ -57,42 +75,21 @@ class autoload
 			require_once $pathToFile;
 		}
 
+
+		if(class_exists($class_name) === false)
+		{
+			throw new \Exception('Error: class '.$class_name.' not found');
+		}
+
 		if($isStatic === false)
 		{
-			if(!empty($namespace))
-			{
-				$class_name	=	$namespace.$class_name;
-
-				if(class_exists($class_name) === false)
-				{
-					throw new \Exception('class '.$class_name.'('.$pathToFile.') not exists (with namespace)');
-				}
-
-				return new $class_name($parameter);
-			}
-			else
-			{
-				if(class_exists($class_name) === false)
-				{
-					throw new \Exception('class '.$class_name.'('.$pathToFile.') not exists');
-				}
-
-				return new $class_name($parameter);
-			}
+			return new $class_name($parameter);
 		}
 		else
 		{
-			if(!empty($namespace))
-			{
-				$class_name	=	$namespace.$class_name;
-			}
+			new $class_name();
 
-			if(class_exists($class_name) === false)
-			{
-				new $class_name();
-			}
+			return true;
 		}
-
-		return true;
     }
 }
