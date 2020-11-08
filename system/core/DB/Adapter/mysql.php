88da6e0652f13f9641da98305efbd8f28df1b5ed
Robbyn Gerhardt <r.gerhardt@team-braeutigam.de>
2020-11-08 20:09:41 +0100

    - cleanup code

--- system/core/DB/Adapter/mysql.php
+++ system/core/DB/Adapter/mysql.php
@@ -56,6 +56,9 @@ class mysql implements AdapterInterface
 	 */
 	public function connection(DBConnectionConfig $config)
 	{
+
+
+
 		$this->mysql	=	new \mysqli($config->host, $config->username, $config->password, $config->database, $config->port);
 
 		$this->table_prefix	=	$config->table_prefix;
